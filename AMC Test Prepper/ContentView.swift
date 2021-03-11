a351dc19d06331544d33c6b6a8174683869a8a34
Nathan G <nathan.gooneratne@gmail.com>
2021-03-11 08:34:29 -0500

    Initial Commit

---
+++ AMC Test Prepper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AMC Test Prepper
+//
+//  Created by Nathan Gooneratne (student LM) on 3/11/21.
+//  Copyright © 2021 Nathan Gooneratne (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
