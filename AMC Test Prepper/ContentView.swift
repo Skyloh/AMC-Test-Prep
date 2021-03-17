55660eaafd4388a12bcf5d934fad89265dcbba38
Nathan Cengel <s015415@studnets.lmsd.org>
2021-03-17 11:32:14 -0400

    11:31 mar17

--- AMC Test Prepper/ContentView.swift
+++ AMC Test Prepper/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("yayyayya it workedbeans")
+        Text("yayyayya it worked, Bean battles - bean ba pow :")
     }
 }
 
