da2a2a1595db76602814acf3b559fa39db4f6fe1
ulrichboromir <alexbnikolov@gmail.com>
2021-04-07 08:54:21 -0400

    alex update UI

---
+++ AMC Test Prepper/homeView.swift
@@ -0,0 +1,21 @@
+//
+//  homeView.swift
+//  AMC Test Prepper
+//
+//  Created by Alexander Nikolov (student LM) on 4/6/21.
+//  Copyright © 2021 Nathan Gooneratne (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct homeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct homeView_Previews: PreviewProvider {
+    static var previews: some View {
+        homeView()
+    }
+}
