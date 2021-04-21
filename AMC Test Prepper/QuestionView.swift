8e5db2cdde8571af2205d14f6ce63ea5faa0227c
ulrichboromir <alexbnikolov@gmail.com>
2021-04-21 08:52:30 -0400

    question view update, outline

--- AMC Test Prepper/QuestionView.swift
+++ AMC Test Prepper/QuestionView.swift
@@ -11,32 +11,57 @@ import SwiftUI
 //This is the flashcard view (main question mode)
 struct QuestionView: View {
     
-    @State var question = Question(site: "https://artofproblemsolving.com/wiki/index.php/2018_AMC_8_Problems/Problem_1")
-     
-     var body: some View {
+    @State var question = Question()
+    
+    var body: some View {
         
+        ZStack{
+            
+            Rectangle().fill(Color("color4"))
+            
+            VStack{
+                
+                //Displaying question
+                Text(question.problemText).foregroundColor(Color("color1"))
+                question.loadFromIndex(index:0).resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 200)
+                question.loadFromIndex(index:1).resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 100)
+                
+                Spacer()
+                
+                
+                //Show Answer
+                Button(action: {
+                    print(self.question.solutionText)
+                    Text(self.question.solutionText)
+                }) {
+                    Text("Show solution")
+                        .frame(width: 300)
+                        .padding(.vertical, 20)
+                        .background(Color("color3"))
+                        .cornerRadius(8)
+                        .foregroundColor(Color("color2"))
+                }
+                
+                Spacer()
+                
+                //Next Question
+                Button(action: {
+                    print(self.question.formatTextWithImages())
+                    self.question = Question()
+                }) {
+                    Text("Next Question")
+                        .frame(width: 200)
+                        .padding(.vertical, 20)
+                        .background(Color("color3"))
+                        .cornerRadius(8)
+                        .foregroundColor(Color("color2"))
+                }
+                
+            }
+            
+        }
         
-         VStack{
-            
-            //Question
-            Text("Question Here")
-            
-            /*
-             Text(question.problemText)
-             question.loadFromIndex(index: 2).resizable().aspectRatio(contentMode: .fit)
-             Text(question.solutionText)
-            */
-            
-            
-            //Next Question button
-             Button("Next", action: {
-                 self.question = Question()
-             })
-            
-            //Show Answer
-        
-         }
-     }
+    }
 }
 
 struct QuestionView_Previews: PreviewProvider {
