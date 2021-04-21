c2bba40a37d6d3ba21210dafa750fe480e9c9e41
ulrichboromir <alexbnikolov@gmail.com>
2021-04-21 09:38:33 -0400

    showing solution now working

--- AMC Test Prepper/QuestionView.swift
+++ AMC Test Prepper/QuestionView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct QuestionView: View {
     
     @State var question = Question()
+    @State var ans = "eeee"
     
     var body: some View {
         
@@ -23,18 +24,24 @@ struct QuestionView: View {
                 
                 //Displaying question
                 Text(question.problemText).foregroundColor(Color("color1"))
-                question.loadFromIndex(index:0).resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 200)
-                question.loadFromIndex(index:1).resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 100)
+                question.loadFromIndex(index:0).resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 50)
+                question.loadFromIndex(index:1).resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 50)
                 
                 Spacer()
                 
                 
                 //Show Answer
+                Text(ans)
+                    .frame(width: 300)
+                    .padding(.vertical, 50)
+                    .cornerRadius(8)
+                    .foregroundColor(Color("color1"))
+                
                 Button(action: {
                     print(self.question.solutionText)
-                    Text(self.question.solutionText)
+                    self.ans = self.question.solutionText
                 }) {
-                    Text("Show solution")
+                    Text("Show Solution")
                         .frame(width: 300)
                         .padding(.vertical, 20)
                         .background(Color("color3"))
@@ -48,6 +55,7 @@ struct QuestionView: View {
                 Button(action: {
                     print(self.question.formatTextWithImages())
                     self.question = Question()
+                    self.ans = "aaa"
                 }) {
                     Text("Next Question")
                         .frame(width: 200)
